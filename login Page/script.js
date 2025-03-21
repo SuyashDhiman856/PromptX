0cf7909c345d211edd7db453bf63a722a855f993
Shashank_Kumar <shashankmuz3@gmial.com>
2025-03-21 18:25:53 +0530

    added logo

--- login Page/script.js
+++ login Page/script.js
@@ -8,20 +8,17 @@ document.getElementById('show-login').addEventListener('click', function() {
     document.querySelector('.login-container').style.display = 'block';
 });
 document.getElementById('lg_in').addEventListener('click', function(event) {
-    event.preventDefault(); // Prevent default form submission
+    event.preventDefault();
 
-    // Get input values
     let email = document.querySelector('input[type="email"]').value;
     let password = document.querySelector('input[type="password"]').value;
 
-    // Define valid credentials
     const validEmail = "shashankmuz3@gmail.com";
     const validPassword = "7410";
 
-    // Check if credentials match
     if (email === validEmail && password === validPassword) {
-        window.location.href = '../Home_Page/index.html'; // Redirect on success
+        window.location.href = '../Home_Page/index.html';
     } else {
-        alert("Invalid email or password!"); // Show error message
+        alert("Invalid email or password!"); 
     }
 });
